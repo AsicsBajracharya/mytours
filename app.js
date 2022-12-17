97f3f3c1bab40b42d4cf09648accd4027dd2bc3b
Ashish <machiavellian2333@gmail.com>
2022-12-18 01:13:44 +0545

    creating custom middlewares

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.use(express.json());
 //   res.status(200).json({ message: 'the home page of an app' });
 // });
 
+//CUSTOM MIDDLEWARE
+app.use((req, res, next) => {
+  console.log('hello from the middleware');
+  next();
+});
+
+//MANIPULATING REQ FROM CUSTOM MIDDLEWARE
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 //READ THE FILE FROM DEV-DATA
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -21,6 +32,7 @@ app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours?.length,
+    requestedAt: req.requestTime,
     data: {
       tours,
     },
@@ -72,6 +84,15 @@ app.delete('/api/v1/tours/:id', (req, res) => {
   res.send('delete request goes here');
 });
 
+//CUSTOM MIDDLEWARE
+app.use((req, res, next) => {
+  console.log('hello from the middleware');
+  next();
+});
+
+//A BETTER WAY TO HANDLE ROUTES
+// app.route('/api/v1.tours').get(getAllTours).post(createTour)
+// app.route('/api/v1/tours/:id').get(getATour).patch(updateTour).delete(deleteTour)
 //STARTING THE SERVER
 app.listen(8000, () => {
   console.log('app running at port 8000');
