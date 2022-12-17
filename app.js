14aa6617ab6bbfdba80e54b89c45e1fd10cdf1ad
Ashish <machiavellian2333@gmail.com>
2022-12-17 17:02:17 +0545

    read local file success, handling get all tours completed

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const fs = require('fs');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'the home page of an app' });
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'the home page of an app' });
+// });
+
+//READ THE FILE FROM DEV-DATA
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//GET ALL TOUR ROUTE
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours?.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 //STARTING THE SERVER
