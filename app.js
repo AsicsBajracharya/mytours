ff463ece7d93d2cd8c6d07180402ddc1697a7fe1
Ashish <machiavellian2333@gmail.com>
2022-12-22 02:41:43 +0545

    error controler created

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const AppError = require('./utils/AppError');
+
+const globalErrorHandler = require('./controllers/errorController');
+
 //lOGGER MIDDLEWARE
 const morgan = require('morgan');
 
@@ -51,17 +55,11 @@ app.all('*', (req, res, next) => {
   err.status = 'fail';
   err.statuscode = 404;
 
-  next(err);
+  // next(err);
+  next(new AppError(`can't find ${req.originalUrl}`, 404));
 });
 
 //GLOBAL ERROR HANDLER MIDDLEWARE
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
