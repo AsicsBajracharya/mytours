560bbaa04c6b6424ca81494ed252a8cac0ddb6e2
Ashish <machiavellian2333@gmail.com>
2022-12-22 03:18:45 +0545

    handling errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  } else {
+    //1 log error
+    console.error('ERROR', err);
+    //2 send generic message
+    res.status(500).json({
+      status: 'errorGeneric',
+      message: 'something went wrong',
+    });
+  }
+};
 //GLOBAL ERROR HANDLER MIDDLEWARE
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.ENV == 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.ENV == 'production') {
+    sendErrorProd(err, res);
+  }
 };
