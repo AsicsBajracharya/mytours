b71947b596b5e4281a6bdcd4511e69d93c3e9173
Ashish <machiavellian2333@gmail.com>
2022-12-22 02:28:20 +0545

    global error handling middleware added

--- app.js
+++ app.js
@@ -43,9 +43,24 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `can't find ${req.originalUrl}`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `can't find ${req.originalUrl}`,
+  // });
+  const err = new Error(`can't find ${req.originalUrl}`);
+  err.status = 'fail';
+  err.statuscode = 404;
+
+  next(err);
+});
+
+//GLOBAL ERROR HANDLER MIDDLEWARE
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
