7fd670f1057f54eeff2ed287e2327e6bfafa00e1
Ashish <machiavellian2333@gmail.com>
2022-12-22 04:31:47 +0545

    creating users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,14 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
 // ROUTERS
 // DECLEARING ROUTERS
 const router = express.Router();
 
 //USING ROUTERS
+
+router.post('/signup', signup);
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
