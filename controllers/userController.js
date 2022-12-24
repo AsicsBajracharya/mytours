baef667ae185391ed6cbc16d46bdf5b6de852384
Ashish <machiavellian2333@gmail.com>
2022-12-24 23:14:35 +0545

    update me completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
+
+//FUNCTIONS
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 // HANDLERS
 
@@ -9,6 +19,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     status: 'success',
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1) CREATE ERROR IF USER POSTS PASSWORD DATA
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates, please use /updatePassword',
+        400
+      )
+    );
+  }
+
+  //2) UPDATE USER DOCUMENT
+  const fileteredBody = filterObj(req.body, ['name', 'email']);
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, fileteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
