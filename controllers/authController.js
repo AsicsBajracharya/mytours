f3a897b9b9d8d751ba13d8de73d03cf5f15d4b88
Ashish <machiavellian2333@gmail.com>
2022-12-24 12:58:53 +0545

    forgot password functionality created

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const { promisify } = require('util'); //BUILT IN FUNCTION
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
+const sendEmail = require('../utils/email');
 const jwt = require('jsonwebtoken');
 
 const signToken = (id) => {
@@ -175,6 +176,37 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
+
+  // 3) SEND TO SUER VIA EMAIL
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email',
+    });
+  } catch (e) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    console.log(user, 'user from the forgot password');
+    return next(
+      new AppError(
+        'there was an error while sending the email. please try again later',
+        500
+      )
+    );
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
