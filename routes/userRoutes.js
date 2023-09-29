c17e085d51a5c47d2462c863a6a3501111d9a4eb
Ashish <machiavellian2333@gmail.com>
2023-09-29 11:36:38 +0545

    protect middleware added in sequence

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -32,11 +32,13 @@ router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
-router.patch('/updatePassword', protect, updatePassword);
+router.use(protect);
 
-router.get('/me', protect, getMe, getUser);
-router.patch('/updateMe', protect, updateMe);
-router.delete('/deleteMe', protect, deleteMe);
+router.patch('/updatePassword', updatePassword);
+
+router.get('/me', getMe, getUser);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
